751d4d6351199d99e6b24a77a18c47e99a556fa7
RavinduVinoj <117930279+Ravinduvinoj@users.noreply.github.com>
2024-04-05 07:43:10 +0530

    added routing for admin new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { JCategoryComponent } from './j-category/j-category.component';
 import { AccountsComponent } from './admin/accounts/accounts.component';
+import { JobapprovalComponent } from './admin/jobapproval/jobapproval.component';
+import{ PaymentComponent } from './admin/payment/payment.component';
+import { SubscriptionComponent } from './admin/subscription/subscription.component';
 
+let usertype = 'admin';
+if(usertype=='admin'){
+  
+}
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
  {path: 'dashboard', component: DashboardComponent},
  {path: 'j-category', component: JCategoryComponent},
- {path: 'accounts', component: AccountsComponent}
+ {path: 'accounts', component: AccountsComponent},
+ {path: 'jobapproval', component: JobapprovalComponent},
+ {path: 'payment', component: PaymentComponent},
+ {path: 'subscriptions', component: SubscriptionComponent},
 
 ];
 
