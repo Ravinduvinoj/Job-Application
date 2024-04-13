66a719d05458e62c947bdc91cfc609d5fc366757
RavinduVinoj <117930279+Ravinduvinoj@users.noreply.github.com>
2024-04-13 23:36:52 +0530

    strictPropertyInitialization and remove form actions

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+submit() {
+throw new Error('Method not implemented.');
+}
 
 }
