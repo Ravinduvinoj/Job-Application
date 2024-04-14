5ecfe6e2fb829590af643228bdf2ff977625322a
RavinduVinoj <117930279+Ravinduvinoj@users.noreply.github.com>
2024-04-15 03:16:22 +0530

    do the registration

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { navigationData } from './nav-data';
+import { Emitter } from '../../emitter/emitter';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 interface navToggle {
   screenWidth: number;
@@ -10,11 +13,34 @@ interface navToggle {
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   title = 'Navbar';
 
   @Output() onTogglenav: EventEmitter<navToggle> = new EventEmitter();
 
   collapsed = false;
   naviData = navigationData;
+
+  message : any;
+
+  constructor (private http:HttpClient,
+    private snackBar: MatSnackBar,
+  ){
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/user', {
+    withCredentials:true,
+    }).subscribe(
+      (res: any)=> {
+      this.message = `${res.firstname}`;
+      Emitter.authEmitter.emit(true)
+    },
+    (err)=>{
+      this.message = "you are not logged"
+      Emitter.authEmitter.emit(false)
+    }
+  );
+    
+}
 }
--- src/app/modules/sidenav/sidenav.component.ts
+++ src/app/modules/sidenav/sidenav.component.ts
@@ -1,39 +1,54 @@
-import { Component, Output ,EventEmitter, OnInit ,} from '@angular/core';
-import { adminData, empData} from './side-data';
-
+import { Component, Output, EventEmitter, OnInit, } from '@angular/core';
+import { adminData, empData } from './side-data';
+import { HttpClient } from '@angular/common/http';
+import { Emitter } from '../../emitter/emitter';
 
 interface SidenavToggle {
-  screenWidth : number;
+  screenWidth: number;
   collapsed: boolean;
 }
-
-
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
-@Output() onToggleSidenav: EventEmitter<SidenavToggle> = new EventEmitter();
+
+  @Output() onToggleSidenav: EventEmitter<SidenavToggle> = new EventEmitter();
 
   collapsed = false;
-  screenWidth =0;
+  screenWidth = 0;
   navData = adminData;
 
-// ngOnInit(): void {
-//     this.screenWidth = window.innerWidth;
-// }
-
-  toggleCollapse():void {
-    this.collapsed= !this.collapsed;
-    this.onToggleSidenav.emit({collapsed:this.collapsed,screenWidth:this.screenWidth})
-   
-  }
-
-  closeSidenav(): void{
-    this.collapsed=false;
-    this.onToggleSidenav.emit({collapsed:this.collapsed,screenWidth:this.screenWidth})
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSidenav.emit({ collapsed: this.collapsed, screenWidth: this.screenWidth })
 
   }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSidenav.emit({ collapsed: this.collapsed, screenWidth: this.screenWidth })
+
+  }
+  authenticated: boolean;
+
+
+  constructor(private http: HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    Emitter.authEmitter.subscribe((auth: boolean) => {
+      this.authenticated = auth;
+    })
+  }
+
+  logout(): void {
+    this.http.post('http://localhost:5000/api/logout',{},
+      {withCredentials:true}
+    ).subscribe(()=> this.authenticated = false)
+  }
+
+
 }
