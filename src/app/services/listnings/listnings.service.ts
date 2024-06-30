7c7e3e780ec7ff767bfcbbafed94dbf885308c0d
Ravinduvinoj <ravinduvinoj100@gmail.com>
2024-06-30 20:53:13 +0530

    and handeled other 2 errors

--- src/app/services/listnings/listnings.service.ts
+++ src/app/services/listnings/listnings.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,16 +14,33 @@ export class ListningsService {
   constructor(private http: HttpClient) { }
 
   getjobapp(): Observable<any> {
-    return this.http.get<any>(`${this.apiurl}/${this.appData._id}`);
+    return this.http.get<any>(`${this.apiurl}/${this.appData._id}`).pipe(
+      catchError(this.handleError)
+    )
   }
   getHistoryapp(): Observable<any> {
-    return this.http.get<any>(`${this.apiurl2}/${this.appData._id}`);
+    return this.http.get<any>(`${this.apiurl2}/${this.appData._id}`).pipe(
+      catchError(this.handleError)
+    )
   }
   setJobData(data: any) {
     this.appData = data;
   }
   getPostInfo() {
     return this.appData;
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
 
