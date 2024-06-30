5ecd1c5a64061f3622e71e492f1884ee9699fcdc
Ravinduvinoj <ravinduvinoj100@gmail.com>
2024-06-30 20:45:04 +0530

    error hndled displapost api

--- src/app/services/jobapprove/jobapproval.service.ts
+++ src/app/services/jobapprove/jobapproval.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 
 @Injectable({
@@ -13,7 +13,9 @@ export class JobapprovalService {
   constructor(private http: HttpClient) { }
 
   getjobpost(): Observable<any> {
-    return this.http.get<any>(this.apiurl);
+    return this.http.get<any>(this.apiurl).pipe(
+      catchError(this.handleError)
+    )
   }
  
   setJobData(data: any) {
@@ -21,5 +23,19 @@ export class JobapprovalService {
   }
   getJobData() {
     return this.jobData;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
